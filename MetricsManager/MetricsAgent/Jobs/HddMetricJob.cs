6f651c87d595a52ed0394e714f9d2d98d81f7c98
splinter-07 <splinter_07@list.ru>
2021-05-19 14:28:22 +0300

    lesson-5

---
+++ MetricsManager/MetricsAgent/Jobs/HddMetricJob.cs
@@ -0,0 +1,34 @@
+﻿using MetricsAgent.DAL;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class HddMetricJob : IJob
+    {
+        private IHddMetricsRepository _repository;
+        private PerformanceCounter _hddCounter;
+
+        public HddMetricJob(IHddMetricsRepository repository)
+        {
+            _repository = repository;
+            _hddCounter = new PerformanceCounter("Cache", "Dirty Pages");
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            var hddUsageInPercents = Convert.ToInt32(_hddCounter.NextValue());
+
+            var time = DateTimeOffset.UtcNow;
+
+            _repository.Create(new Models.HddMetric { Time = time, Value = hddUsageInPercents });
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ MetricsManager/MetricsAgent/Jobs/RamMetricJob.cs
@@ -0,0 +1,34 @@
+﻿using MetricsAgent.DAL;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class RamMetricJob : IJob
+    {
+        private IRamMetricsRepository _repository;
+        private PerformanceCounter _ramCounter;
+
+        public RamMetricJob(IRamMetricsRepository repository)
+        {
+            _repository = repository;
+            _ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            var ramUsageInPercents = Convert.ToInt32(_ramCounter.NextValue());
+
+            var time = DateTimeOffset.UtcNow;
+
+            _repository.Create(new Models.RamMetric { Time = time, Value = ramUsageInPercents });
+
+            return Task.CompletedTask;
+        }
+    }
+}
